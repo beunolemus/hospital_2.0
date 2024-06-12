50ab625850e234e2e746a763a406044b690dc893
Diego Oliver Basilio <162365937+Diego210436@users.noreply.github.com>
2024-06-12 16:27:37 -0600

    Se añadio la path para RegisterC

---
+++ index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import RegisterUser from '@/components/registerUser.vue'
+import LoginVuew from '@/components/login.vue'
+import dashboard from '@/components/dashboard.vue'
+import Persona from '@/components/persona.vue'
+import RegisterC from '@/components/RegisterC.vue'
+
+
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginVuew
+    },
+   {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: dashboard,
+      children:[
+        {path:'/persona',name:'persona', component : Persona},
+        {path:"/registerC",name:"registerC",component:RegisterC}
+      ]
+    }
+  ]
+})
+
+export default router
