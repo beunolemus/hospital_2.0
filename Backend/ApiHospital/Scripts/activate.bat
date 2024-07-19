0101f9273da7d0ac858efcb1a985ace89c399d9a
brayan <230893@utxicotepec.edu.mx>
2024-07-18 23:49:40 -0600

    Actualizacion de requerimientos

--- Backend/ApiHospital/Scripts/activate.bat
+++ Backend/ApiHospital/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\PC-15\Desktop\Apihospital\ApiHospital
+set VIRTUAL_ENV=E:\hospital_2.0\Backend\ApiHospital
 
 if not defined PROMPT set PROMPT=$P$G
 
