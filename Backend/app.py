b43e1ba083ee3003785c847fa1bbd74a57ab2af5
Bruno Lemus <brno.lems@gmail.com>
2024-08-08 14:48:48 -0600

    Modificacion del modelo de espacios

--- Backend/app.py
+++ Backend/app.py
@@ -3,7 +3,7 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.userrol import userrol
-from routes.espacios import espacio_router
+from routes.espacios import espacio
 
 app=FastAPI(
     title="Privilege Care S.A. de C.V.",
@@ -13,5 +13,5 @@ app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
-app.include_router(espacio_router)
+app.include_router(espacio)
 print ("Hola bienvenido a mi backend")
--- Backend/routes/espacios.py
+++ Backend/routes/espacios.py
@@ -4,7 +4,7 @@ from typing import List
 from portadortoken import Portador
 import crud.espacios, config.db, schemas.espacios, models.espacios
 
-espacio_router = APIRouter()
+espacio = APIRouter()
 
 models.espacios.Base.metadata.create_all(bind=config.db.engine)
 
@@ -15,30 +15,30 @@ def get_db():
     finally:
         db.close()
 
-@espacio_router.get("/espacios/", response_model=List[schemas.espacios.Espacio], tags=["Espacios"],dependencies=[Depends(Portador())])
+@espacio.get("/espacios/", response_model=List[schemas.espacios.Espacio], tags=["Espacios"],dependencies=[Depends(Portador())])
 def read_espacios(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     db_espacios = crud.espacios.get_espacios(db=db, skip=skip, limit=limit)
     return db_espacios
 
-@espacio_router.get("/espacios/{id}", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
+@espacio.get("/espacios/{id}", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
 def read_espacio(id: int, db: Session = Depends(get_db)):
     db_espacio = crud.espacios.get_espacio(db=db, id=id)
     if db_espacio is None:
         raise HTTPException(status_code=404, detail="Espacio no encontrado")
     return db_espacio
 
-@espacio_router.post("/espacios/", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
+@espacio.post("/espacios/", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
 def create_espacio(espacio: schemas.espacios.EspacioCreate, db: Session = Depends(get_db)):
     return crud.espacios.create_espacio(db=db, espacio=espacio)
 
-@espacio_router.put("/espacios/{id}", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
+@espacio.put("/espacios/{id}", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
 def update_espacio(id: int, espacio: schemas.espacios.EspacioUpdate, db: Session = Depends(get_db)):
     db_espacio = crud.espacios.update_espacio(db=db, id=id, espacio=espacio)
     if db_espacio is None:
         raise HTTPException(status_code=404, detail="Espacio no encontrado")
     return db_espacio
 
-@espacio_router.delete("/espacios/{id}", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
+@espacio.delete("/espacios/{id}", response_model=schemas.espacios.Espacio, tags=["Espacios"],dependencies=[Depends(Portador())])
 def delete_espacio(id: int, db: Session = Depends(get_db)):
     db_espacio = crud.espacios.delete_espacio(db=db, id=id)
     if db_espacio is None:
