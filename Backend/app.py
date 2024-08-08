c209b4b0c5cb7fc91c37cf7451e649185d845e4f
Brayan <230893@utxicotepec.edu.mx>
2024-08-08 13:21:46 -0600

    creacion de api de la tabla cirugias

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,7 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.userrol import userrol
+from routes.cirugia import cirugia_router
 
 app=FastAPI(
     title="Privilege Care S.A. de C.V.",
@@ -12,4 +13,5 @@ app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
 app.include_router(userrol)
+app.include_router(cirugia_router)
 print ("Hola bienvenido a mi backend")
